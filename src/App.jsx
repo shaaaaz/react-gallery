33a9a39c275373fcff8bb5ccb0face5358a43e56
shaaaaz <jiwanishaaz@gmail.com>
2024-01-15 14:19:32 +0530

    push

--- src/App.jsx
+++ src/App.jsx
@@ -23,8 +23,29 @@ const imageData = ()=>{
   return data;
 }
 
-function App() {
+
+
+function App(imgData) {
   // code here
+  
+  const images = imageData();
+  console.log(images);
+  
+  return(
+    <div>
+  
+      <h1>Kalvium Gallery</h1>
+
+      <div id='grid'> 
+          {images.map((element) => (
+          <img src={element.img} id={element.id}/>
+        
+          ))};
+      </div>
+   
+    </div>
+ )
+
 }
 
 export default App;
--- src/AppClass.jsx
+++ src/AppClass.jsx
@@ -26,5 +26,24 @@ export default class AppClass extends Component{
     return data;
   }
 
-  // code here
+  render(){
+    const images = this.imageData();
+
+    return(
+      <div>
+    
+        <h1>Kalvium Gallery</h1>
+
+        <div id='grid'> 
+            {images.map((element) => (
+            <img src={element.img} id={element.id}/>
+          
+            ))};
+        </div>
+     
+      </div>
+   )
+  }
+
+
 }
